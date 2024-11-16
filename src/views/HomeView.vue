fc47d5c3c518d49919297d47a83b23c128ba262e
Ulysses <tranquilinoulysses9@gmail.com>
2024-11-16 10:00:00 +0800

    add ts

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,22 @@
 <template>
-  <div class="home">HOME</div>
+  <div class="home">
+    HOME
+    <p ref="paragraph">My nbame is {{ name }} and my age is {{ age }}</p>
+    <button @click="handleClick">CLIcK me</button>
+  </div>
 </template>
 
-<script>
-import { defineComponent } from "vue";
+<script setup lang="ts">
+import { ref } from "vue";
 
-export default defineComponent({
-  name: "HomeView",
-  components: {},
-  setup() {
-    console.log("setuip");
-  },
-});
+const paragraph = ref<HTMLElement | null>(null);
+
+const name: string = "ulysses";
+const age: number = 19;
+
+const handleClick = (): void => {
+  console.log("You clicked me! ", paragraph, paragraph.value);
+  paragraph.value.classList.add("test");
+  paragraph.value.textContent = "Hello ";
+};
 </script>
